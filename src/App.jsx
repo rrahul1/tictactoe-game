0d9aef70dedbad39fe765f2f2935be1112327551
Rahul Singh <rathore.rahul.1999@gmail.com>
2023-05-20 23:54:44 +0530

    done

--- src/App.jsx
+++ src/App.jsx
@@ -78,6 +78,7 @@ function App() {
         Current Game History
       </h2>
       <History history={history} moveTo={moveTo} currentMove={currentMove} />
+      <div className="bg-balls"></div>
     </div>
   );
 }
